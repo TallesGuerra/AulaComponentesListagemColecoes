cf25d4f13e42f162f74593678b4c9f09d9dd9d67
Talles Guerra <talles-guerra@hotmail.com>
2026-02-10 15:22:03 +0000

    funcoes criada com class Matematica e fun somar. Explicando a diferença da fun dentro e fora da classe

--- app/src/main/java/com/example/aulacomponenteslistagemcolecoes/teste/funcoes.kt
+++ app/src/main/java/com/example/aulacomponenteslistagemcolecoes/teste/funcoes.kt
@@ -1,5 +1,23 @@
 package com.example.aulacomponenteslistagemcolecoes.teste
 
+class Matematica {
+    fun somar(n1: Int, n2: Int): Int { /* MetoDo » Criando a fun detntro da classe ela precisa ser instanciada*/
+        return n1 + n2
+    }
+}
+
+// FUNÇÃO » Crianddo a funçao fora da classe pode ser chamada em qualquer lugar sem instanciar
+fun somar(n1: Int, n2: Int): Int {
+    return n1 + n2
+}
+
+
 fun main() {
-    
+   /* val matematica = Matematica()
+    val retorno = matematica.somar(10, 10)*/ // utilizando a fun q veido da class Matematica
+
+    val retorno = somar(10, 10) // utilizando a fun somar fora da classe
+
+    println("O retorno é $retorno")
+
 }
