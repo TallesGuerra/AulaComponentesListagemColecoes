90826aa47e585ef0726634565edeec8cb0f708fa
Talles Guerra <talles-guerra@hotmail.com>
2026-02-11 20:00:15 +0000

    Data class

--- app/src/main/java/com/example/aulacomponenteslistagemcolecoes/teste/classes.kt
+++ app/src/main/java/com/example/aulacomponenteslistagemcolecoes/teste/classes.kt
@@ -1,13 +1,16 @@
 package com.example.aulacomponenteslistagemcolecoes.teste
 
+/*
 class Motorista(val nome: String){
     fun exibirDadosMotorista() = println("Motorista: $nome")
 
-    /* class Caminhao(val nomeCaminhao: String){ // Classe aninhada(Nested Class), pois tem relacao com a classe motorista
+
+/* class Caminhao(val nomeCaminhao: String){ // Classe aninhada(Nested Class), pois tem relacao com a classe motorista
         fun exibirDadosCaminhao() = println("Caminhao")
 
 
     }*/
+
 
     inner class Caminhao(val nomeCaminhao: String){ // Inner class tem comunicação com a classe motorista
         fun exibirDadosCaminhao() = println("Caminhao: $nomeCaminhao, motorista: $nome")
@@ -17,9 +20,24 @@ class Motorista(val nome: String){
 
 
 }
+*/
+
+data class Pergunta( var pergunta: String,  var respostaCerta: Int )
+
 
 
 fun main() {
+    val pergunta1 = Pergunta("Qual a pergunta?", 1)
+    val pergunta2 = Pergunta("Qual a pergunta?", 1)
+
+    println(pergunta1 == pergunta2) // Elas vao ser diferentes pq cada um tem uma "identificação."
+
+
+
+
+
+
+/*
 
     val motorista = Motorista("Batata")
     val caminhao = motorista.Caminhao("Carreta Furacão")
@@ -28,17 +46,16 @@ fun main() {
 
 
 
-
-  /*
     // Exemplo classe aninhada (NESTED CLASS)
     val caminhao = Motorista.Caminhao("Carreta Furacão")
-    caminhao.exibirDadosCaminhao()*/
+    caminhao.exibirDadosCaminhao()
 
 
 
 
-   /* val motorista = Motorista("Batata")
-    motorista.exibirDadosMotorista()*/
+    val motorista = Motorista("Batata")
+    motorista.exibirDadosMotorista()
+*/
 
 
 
